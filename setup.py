699e7aa111b40c08be481d4e01fed25ad6ec8a78
Alexey Rusakov <rusakltd@gmail.com>
2025-10-15 09:32:34 +0300

    version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api_lib",
-    version="0.12",
+    version="0.12.1",
     packages=find_packages(),  # <- добавлена запятая
     install_requires=[
         "requests",  # <- в кавычках
